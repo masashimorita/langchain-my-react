ee54be6fec184b306c909ed6d3d444f4ca7dbf9f
masashimorita <masashi.morita.mm@gmail.com>
2025-01-11 16:53:46 +0900

    :+1: Created an simple agent tool to calculate the length of a text

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from dotenv import load_dotenv
+from langchain.agents import tool
 
 
 load_dotenv()
 
 
+@tool
+def get_text_length(text: str) -> int:
+    """Returns a length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"')
+    
+    return len(text)
+
+
 if __name__ == '__main__':
     print('HELLO ReAct Langchain!')
+    print(get_text_length.invoke(input={"text": "Dog"}))
