f3bbfc3b630194a56fb7f37a612816edaad6e5c8
masashimorita <masashi.morita.mm@gmail.com>
2025-01-11 17:01:09 +0900

    :sparkles: Define the tools & template for ReAct agent to be used

--- main.py
+++ main.py
@@ -10,10 +10,33 @@ def get_text_length(text: str) -> int:
     """Returns a length of a text by characters"""
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip('"')
-    
+
     return len(text)
 
 
 if __name__ == '__main__':
     print('HELLO ReAct Langchain!')
-    print(get_text_length.invoke(input={"text": "Dog"}))
+    
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:{agent_scratchpad}
+    """
