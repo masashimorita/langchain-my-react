7ecbb91371d8f638a7699710c15cb27823d9783e
masashimorita <masashi.morita.mm@gmail.com>
2025-01-11 17:49:49 +0900

    :sparkles: Implemented an agent flow of getting input, decide which tool to use, execute tool

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from dotenv import load_dotenv
+from typing import Union, List
 from langchain.agents import tool
+from langchain.agents.output_parsers.react_single_input import (
+    ReActSingleInputOutputParser,
+)
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain_core.tools.render import render_text_description
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_openai import ChatOpenAI
 
 
 load_dotenv()
@@ -14,9 +23,17 @@ def get_text_length(text: str) -> int:
     return len(text)
 
 
-if __name__ == '__main__':
-    print('HELLO ReAct Langchain!')
-    
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+
+    raise ValueError(f"Tool with name {tool_name} not found")
+
+
+if __name__ == "__main__":
+    print("HELLO ReAct Langchain!")
+
     tools = [get_text_length]
 
     template = """
@@ -38,5 +55,28 @@ if __name__ == '__main__':
     Begin!
 
     Question: {input}
-    Thought:{agent_scratchpad}
+    Thought:
     """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(list(tools)),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    agent = (
+        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+    )
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {"input": "What is the length in characters of the text Dog ?"}
+    )
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
